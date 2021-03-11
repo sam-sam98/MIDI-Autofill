df1c1da83650344b5920ea5db8963d104a31b9ba
Mason Ashbridge <masonjash@gmail.com>
2021-03-11 14:38:54 -0500

    Add more test cases and generate summary.csv

--- magenta-trials/index.js
+++ magenta-trials/index.js
@@ -1,49 +1,91 @@
 require('@tensorflow/tfjs-node');
-const { performance } = require('perf_hooks');
 const mrnn = require('@magenta/music/node/music_rnn');
 const mvae = require('@magenta/music/node/music_vae');
 const core = require('@magenta/music/node/core');
 const dayjs = require('dayjs');
 const fs = require('fs');
 const b = require('benny');
+const Papa = require('papaparse');
 
 
 TWINKLE_TWINKLE = {
-    notes: [
-        { pitch: 60, startTime: 0.0, endTime: 0.5 },
-        { pitch: 60, startTime: 0.5, endTime: 1.0 },
-        { pitch: 67, startTime: 1.0, endTime: 1.5 },
-        { pitch: 67, startTime: 1.5, endTime: 2.0 },
-        { pitch: 69, startTime: 2.0, endTime: 2.5 },
-        { pitch: 69, startTime: 2.5, endTime: 3.0 },
-        { pitch: 67, startTime: 3.0, endTime: 4.0 },
-        { pitch: 65, startTime: 4.0, endTime: 4.5 },
-        { pitch: 65, startTime: 4.5, endTime: 5.0 },
-        { pitch: 64, startTime: 5.0, endTime: 5.5 },
-        { pitch: 64, startTime: 5.5, endTime: 6.0 },
-        { pitch: 62, startTime: 6.0, endTime: 6.5 },
-        { pitch: 62, startTime: 6.5, endTime: 7.0 },
-        { pitch: 60, startTime: 7.0, endTime: 8.0 },
+    notes: [{
+            pitch: 60,
+            startTime: 0.0,
+            endTime: 0.5
+        },
+        {
+            pitch: 60,
+            startTime: 0.5,
+            endTime: 1.0
+        },
+        {
+            pitch: 67,
+            startTime: 1.0,
+            endTime: 1.5
+        },
+        {
+            pitch: 67,
+            startTime: 1.5,
+            endTime: 2.0
+        },
+        {
+            pitch: 69,
+            startTime: 2.0,
+            endTime: 2.5
+        },
+        {
+            pitch: 69,
+            startTime: 2.5,
+            endTime: 3.0
+        },
+        {
+            pitch: 67,
+            startTime: 3.0,
+            endTime: 4.0
+        },
+        {
+            pitch: 65,
+            startTime: 4.0,
+            endTime: 4.5
+        },
+        {
+            pitch: 65,
+            startTime: 4.5,
+            endTime: 5.0
+        },
+        {
+            pitch: 64,
+            startTime: 5.0,
+            endTime: 5.5
+        },
+        {
+            pitch: 64,
+            startTime: 5.5,
+            endTime: 6.0
+        },
+        {
+            pitch: 62,
+            startTime: 6.0,
+            endTime: 6.5
+        },
+        {
+            pitch: 62,
+            startTime: 6.5,
+            endTime: 7.0
+        },
+        {
+            pitch: 60,
+            startTime: 7.0,
+            endTime: 8.0
+        },
     ],
     totalTime: 8
 };
 
 
-const rnn_steps = 40;
 const rnn_temperature = 1.5;
 const vae_temperature = 1.5;
-
-const DUPLICATIONS = 200;
-
-let sequences = [];
-
-for (let i = 0; i < DUPLICATIONS; i++) {
-    sequences.push(TWINKLE_TWINKLE);
-}
-
-let large_twinkle_twinkle = core.sequences.concatenate(sequences);
-
-console.log(large_twinkle_twinkle);
 
 function saveNotes(noteSequence) {
     noteSequence.notes.forEach(note => note.velocity = 100);
@@ -69,37 +111,57 @@ function randomizeQuantizedSequence(numNotes, duration, quantization, range) {
         notes: notes,
         totalTime: duration,
     };
-    console.log(sequence);
     return core.sequences.quantizeNoteSequence(sequence, quantization);
 }
 
-function runRnnSuite(name, model, range) {
+function randomNoteCases(model, noteRange, durations, steps, sizes) {
+    return durations.map((duration) => {
+        return steps.map((step) => {
+            return sizes.map((size) => {
+                return b.add(`${size} notes ${step} steps ${duration} duration`, async () => {
+                    let randomNotes = randomizeQuantizedSequence(size, duration, 4, noteRange);
+                    return async () => {
+                        await model.continueSequence(randomNotes, step, rnn_temperature);
+                    };
+                });
+            });
+        })
+    }).flat(Infinity);
+}
+
+function twinkleRepetitions(model, repetitions, steps) {
+    return steps.map((step) => {
+        return repetitions.map((repetition) => {
+            return b.add(`Twinkle ${repetition} repetitions ${step} steps`, async () => {
+                //let randomNotes = randomizeQuantizedSequence(size, duration, 4, noteRange);
+                let sequences = Array(repetition).fill(TWINKLE_TWINKLE);
+                let sequence = core.sequences.quantizeNoteSequence(
+                    core.sequences.concatenate(sequences),
+                    4
+                );
+                return async () => {
+                    await model.continueSequence(sequence, step, rnn_temperature);
+                };
+            });
+        });
+    }).flat(Infinity);
+}
+
+async function runRnnSuite(name, model, range) {
+    await model.initialize();
     return b.suite(name,
-        b.add('TwinkleTwinkle', async () => {
+        b.add('Twinkle', async () => {
+            const rnn_steps = 80;
             let twinkle = core.sequences.quantizeNoteSequence(TWINKLE_TWINKLE, 4);
             await model.continueSequence(twinkle, rnn_steps, rnn_temperature);
             return async () => {
                 await model.continueSequence(twinkle, rnn_steps, rnn_temperature);
             };
         }),
-        b.add('Random 10 notes', async () => {
-            let randomNotes = randomizeQuantizedSequence(10, 8, 4, range);
-            return async () => {
-                await model.continueSequence(randomNotes, rnn_steps, rnn_temperature);
-            };
-        }),
-        b.add('Random 100 notes', async () => {
-            let randomNotes = randomizeQuantizedSequence(100, 8, 4, range);
-            return async () => {
-                await model.continueSequence(randomNotes, rnn_steps, rnn_temperature);
-            };
-        }),
-        b.add('Random 1000 notes', async () => {
-            let randomNotes = randomizeQuantizedSequence(1000, 8, 4, range);
-            return async () => {
-                await model.continueSequence(randomNotes, rnn_steps, rnn_temperature);
-            };
-        }),
+        ...randomNoteCases(model, range, [8, 16, 32, 64, 128], [16], [16]), // Increasing duration
+        ...randomNoteCases(model, range, [8], [16, 32, 64, 128], [16]), // Increasing generation steps
+        ...randomNoteCases(model, range, [8], [16], [16, 32, 64, 128]), // Increasing random note size
+        ...twinkleRepetitions(model, [2, 4, 8], [32]),
         b.save({
             file: name,
             folder: 'benchmarks',
@@ -107,6 +169,65 @@ function runRnnSuite(name, model, range) {
             format: 'csv'
         }),
     );
+}
+
+// Write a summary of each suite to benchmarks/summary.csv
+async function createSummaryCSV(modelCheckpoints) {
+    const papaPromise = (importFile) => new Promise((resolve, reject) => {
+        const file = fs.createReadStream(importFile);
+        Papa.parse(file, {
+            //header: true,
+            complete: function (results) {
+                resolve(results);
+            },
+            error: function (error) {
+                reject(error);
+            }
+        });
+    });
+
+    let csvResults = [];
+    for (const modelCheckpoint of modelCheckpoints) {
+        // Here lies spaghetti...
+        let wroteModelName = false;
+        csvResults.push((await Promise.all(
+            modelCheckpoint.checkpoints.map(checkpoint => papaPromise(`benchmarks/${checkpoint}.csv`))
+        )).map((checkpoint, index) => {
+            // Remove the header
+            checkpoint.data.splice(0, 1);
+            // Remove fields we don't need
+            checkpoint.data.forEach((data) => {
+                [1, 2, 3, 5, 9, 11, 12, 13, 14].reverse().forEach(index => data.splice(index, 1));
+            });
+            // Add a field for the checkpoint name
+            checkpoint.data.forEach((data, cidx) => data.splice(0, 0, cidx == 0 ? modelCheckpoint.checkpoints[index] : ''));
+            // Add a field for the model name.
+            // Only fill out the model name on first row for aesthetics.
+            checkpoint.data.forEach((data, index) => data.splice(0, 0, (!wroteModelName && index == 0) ? modelCheckpoint.model : ''));
+            wroteModelName = true;
+            console.log(checkpoint);
+            return checkpoint;
+        }).flat());
+    }
+
+    const header = ['Model', 'Checkpoint', 'Benchmark', 'Samples', 'Min', 'Max', 'Mean', 'Std. Dev'];
+    let summaryData = [header];
+
+    for (let csv of csvResults) {
+        summaryData = summaryData.concat(csv.map(item => item.data).flat());
+    }
+
+    let summary = {
+        data: summaryData,
+        meta: csvResults[0][0].meta,
+    };
+
+    let outCsv = Papa.unparse(summary);
+
+    console.log(JSON.stringify(summaryData, undefined, 2));
+    console.log(outCsv);
+
+    fs.writeFileSync('benchmarks/summary.csv', outCsv);
 }
 
 (async () => {
@@ -129,7 +250,7 @@ function runRnnSuite(name, model, range) {
     let mel_4bar_small_q2 = new mvae.MusicVAE('https://storage.googleapis.com/magentadata/js/checkpoints/music_vae/mel_4bar_small_q2');
     await mel_4bar_small_q2.initialize();
 
-    await b.suite('MusicVAE',
+    await b.suite('musicvae',
         b.add('10 Samples', async () => {
             return async () => {
                 await mel_4bar_small_q2.sample(10, vae_temperature)
@@ -146,10 +267,22 @@ function runRnnSuite(name, model, range) {
             };
         }),
         b.save({
-            file: 'musicvae',
+            file: 'mel_4bar_small_q2',
             folder: 'benchmarks',
             details: true,
             format: 'csv'
         }),
+    );
+
+    await createSummaryCSV(
+        [{
+                model: 'MusicRNN',
+                checkpoints: ['basic_rnn', 'melody_rnn', 'drum_kit_rnn']
+            },
+            {
+                model: 'MusicVAE',
+                checkpoints: ['mel_4bar_small_q2']
+            },
+        ]
     );
 })();
