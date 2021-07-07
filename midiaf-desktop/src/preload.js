db46d096399789802bc0ed535b75b2fb84365646
Mason <masonjash@gmail.com>
2021-07-07 12:20:41 -0400

    Add UI for companion app

---
+++ midiaf-desktop/src/preload.js
@@ -0,0 +1,10 @@
+import { contextBridge, ipcRenderer } from "electron";
+
+contextBridge.exposeInMainWorld("api", {
+    send: (channel, ...args) => {
+        ipcRenderer.send(channel, args);
+    },
+    on: (channel, func) => {
+        ipcRenderer.on(channel, func);
+    }
+})
